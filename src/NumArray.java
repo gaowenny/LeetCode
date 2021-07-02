13341c46a7e4e06cbbc86ab5e859ebde8f3335a7
gaowenny <wdxhg6@163.com>
2021-07-02 14:03:26 +0800

    历史代码提交

--- src/NumArray.java
+++ src/NumArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //其实跟动态规划没什么关系
 public class NumArray {
     private int[] nSums;
@@ -21,10 +23,29 @@ public class NumArray {
         }
     }
 
+    public void wiggleSort(int[] nums) {
+        int [] newNums = nums.clone();
+        Arrays.sort(newNums);
+        int j = 0;
+        for (int i = newNums.length-1; i>=0; i--){
+            int n = newNums.length/2;
+            if(j<n){
+                nums[j*2+1] = newNums[i];
+            }else{
+                nums[(j-n)*2] = newNums[i];
+            }
+            j++;
+        }
+    }
+
+
     public static void main(String[] args){
-        int[] nums = {-2, 0, 3, -5, 2, -1};
+        int[] nums = {5, 3, 1, 2, 3};
         NumArray o = new NumArray(nums);
-        System.out.println(o.sumRange(2,5));
+        o.wiggleSort(nums);
+        for (int num : nums) {
+            System.out.println(num);
+        }
     }
 }
 
---
+++ src/offer/HighQuality/PrintDigits.java
@@ -0,0 +1,6 @@
+package offer.HighQuality;
+
+public class PrintDigits {
+
+
+}
